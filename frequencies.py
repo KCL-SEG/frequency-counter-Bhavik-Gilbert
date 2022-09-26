bd38743a62b4da8e6724791f18fbdc160ad09ea0
Bhavik Glibert <bhaviklob@gmail.com>
2022-09-26 19:17:06 +0100

    Frequencies Dictionary
    Frequencies method creating dictionary of frequencies.

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for item in items:
+        item_str = str(item)
+
+        if item_str in frequencies.keys():
+            count = frequencies[item_str]
+            frequencies[item_str] = count + 1
+        else:
+            frequencies[item_str] = 1
+
     return frequencies
